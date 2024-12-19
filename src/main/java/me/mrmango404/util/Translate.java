e55216da99bdd49a3c350fe4c05ca508c3c11ab8
Hinogo2210 <hinogo2210@gmail.com>
2024-12-19 18:27:46 -0500

    Added Hex, Multi languages support.

--- src/main/java/me/mrmango404/util/Translate.java
+++ src/main/java/me/mrmango404/util/Translate.java
@@ -1,10 +1,23 @@
 package me.mrmango404.util;
 
-import org.bukkit.ChatColor;
+import net.md_5.bungee.api.ChatColor;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Translate {
 
 	public static String color(String message) {
+
+		Pattern pattern = Pattern.compile("&#[a-fA-F0-9]{6}");
+		Matcher matcher = pattern.matcher(message);
+
+		while (matcher.find()) {
+			String color = message.substring(matcher.start(), matcher.end());
+			message = message.replace(color, net.md_5.bungee.api.ChatColor.of(color.substring(1)) + "");
+			matcher = pattern.matcher(message);
+		}
+
 		return ChatColor.translateAlternateColorCodes('&', message);
 	}
 }
